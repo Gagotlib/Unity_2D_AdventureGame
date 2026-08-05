63a3def370425ea4614b18f929a41bc4761f83a5
Gabriel Gotlib <gagotlib@gmail.com>
2026-08-05 11:28:47 +0200

    add enviroment and decoration  physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,33 @@ using UnityEngine.InputSystem;
 
 
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    // Variables related to player character movement
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movementSpeed = 4.0f;
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + movementSpeed * Time.deltaTime * move;
-        transform.position = position;
+    }
+
+    // Called at fixed intervals. Used for physics updates.
+    void FixedUpdate()
+    {
+        float movementSpeed = 4.0f;
+        Vector2 position = (Vector2)rigidbody2d.position + movementSpeed * Time.deltaTime * move;
+        rigidbody2d.MovePosition(position);
     }
 }
